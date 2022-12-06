f3659cdb1a4472728b50b3d6f75fa16c0a387ed1
xholy6 <blast6xgit@gmail.com>
2022-12-06 14:45:46 +0300

    Исправлено отсутствие показа алеута ошибки загрузки картинки

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -41,7 +41,11 @@ class QuestionFactory: QuestionFactoryProtocol {
             do {
                 imageData = try Data(contentsOf: movie.resizedImageURL)
             } catch {
-                print("Failed to load image")
+                DispatchQueue.main.async() { [weak self] in
+                    guard let self = self else {return}
+                    self.delegate?.didFailToLoadData(with: error)
+                }
+                
             }
             
             let rating = Float(movie.rating) ?? 0
